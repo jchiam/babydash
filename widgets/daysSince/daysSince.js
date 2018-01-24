8fe7be66d3b2331bb972809b21fc84e140897ccf
Jonathan Chiam <jczjchiam@gmail.com>
2018-01-24 17:53:05 +0800

    Implement days-since widget

---
+++ widgets/daysSince/daysSince.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Widget } from 'kitto';
+import moment from 'moment';
+
+import './daysSince.scss';
+
+Widget.mount(class DaysSince extends Widget {
+  constructor(props) {
+    super(props);
+
+    this.state = { days: moment().diff(moment(this.props.date), 'day') };
+    setInterval(this.update.bind(this), 60 * 60 * 1000);
+  }
+
+  update() {
+    this.setState({ days: moment().diff(moment(this.props.date), 'day') });
+  }
+
+  render() {
+    const { days } = this.state;
+    const { className, date } = this.props;
+    const dateMoment = moment(date);
+    return (
+      <div className={className}>
+        <div className="num-days">{days}</div>
+        <span className="heart" role="img" aria-label="heart">❤️</span>
+        <div>{`${dateMoment.date()} - ${dateMoment.month() + 1} - ${dateMoment.year()}`}</div>
+      </div>
+    );
+  }
+});
