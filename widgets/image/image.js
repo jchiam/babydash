dc795fb7dd2629ba72c2c6863ca186a492e8d74e
Jonathan Chiam <jczjchiam@gmail.com>
2018-01-28 21:23:27 +0800

    Add image widgets and fix styling for scores dashboard

--- widgets/image/image.js
+++ widgets/image/image.js
@@ -6,12 +6,20 @@ import './image.scss';
 const placeholder = '/assets/images/placeholder.png';
 
 Widget.mount(class Image extends Widget {
-  image() {
-    return {
-      backgroundImage: `url(${this.props.image || placeholder})`
-    };
+  imageStyle() {
+    const { image, contain } = this.props;
+    const style = { backgroundImage: `url(${this.props.image || placeholder})` };
+    if (image) {
+      style.backgroundImage = `url(${image})`;
+      if (contain === 'true') {
+        style.backgroundSize = 'contain';
+        style.backgroundRepeat = 'no-repeat';
+      }
+    }
+    return style;
   }
+
   render() {
-    return <div style={this.image()} className={this.props.className} />;
+    return <div style={this.imageStyle()} className={this.props.className} />;
   }
 });
