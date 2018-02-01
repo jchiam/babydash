49407183150bdac7f225ec357f74aafa56009547
Jonathan Chiam <jczjchiam@gmail.com>
2018-02-01 22:57:57 +0800

    Add auth token requirement to firebase queries

--- jobs/records.exs
+++ jobs/records.exs
@@ -1,17 +1,23 @@
 use Kitto.Job.DSL
 
+defmodule Firebase do
+    def generate_query(key) do
+        "curl #{System.get_env("FIREBASE_URL")}/#{key}.json?auth=#{System.get_env("FIREBASE_TOKEN")}"
+    end
+end
+
 job :squash,
     every: :hour,
-    command: "curl #{System.get_env("FIREBASE_URL")}/squash.json"
+    command: Firebase.generate_query("squash")
 
 job :table_tennis,
     every: :hour,
-    command: "curl #{System.get_env("FIREBASE_URL")}/table-tennis.json"
+    command: Firebase.generate_query("table-tennis")
 
 job :pokemon_battle,
     every: :hour,
-    command: "curl #{System.get_env("FIREBASE_URL")}/pokemon-battle.json"
+    command: Firebase.generate_query("pokemon-battle")
 
 job :basketball_arcade,
     every: :hour,
-    command: "curl #{System.get_env("FIREBASE_URL")}/basketball-arcade.json"
+    command: Firebase.generate_query("basketball-arcade")
